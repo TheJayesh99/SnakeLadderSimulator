b4c70928b6e9549fe6cb46c0c4dbf4f11df4d8fb
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-22 17:20:31 +0530

    Added : A die to get number between 1-6

--- src/SnakeLadderSimulator/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator/SnakeLadderSimulator.java
@@ -6,7 +6,8 @@ public class SnakeLadderSimulator
  {
 	System.out.println( "WelCome To Snake Ladder Simulator" );
 	int START_POSITION = 0;
-	System.out.println(START_POSITION);
- 
+	
+	int DiceRoll = (int) (Math.floor(Math.random() * 10 ) % 6) + 1 ;
+	System.out.println(DiceRoll);
  }
 }
