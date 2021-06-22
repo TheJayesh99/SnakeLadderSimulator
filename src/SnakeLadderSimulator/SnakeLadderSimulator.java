f448e7e724c6377f01101e659747b4e743e699f0
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-22 17:07:52 +0530

    Updated: Set THe Start Position at 0

--- src/SnakeLadderSimulator/SnakeLadderSimulator.java
+++ src/SnakeLadderSimulator/SnakeLadderSimulator.java
@@ -1,7 +1,12 @@
 package SnakeLadderSimulator;
 
-public class SnakeLadderSimulator {
- public static void main(String[] args) {
+public class SnakeLadderSimulator
+{
+ public static void main(String[] args)
+ {
 	System.out.println( "WelCome To Snake Ladder Simulator" );
-}
+	int START_POSITION = 0;
+	System.out.println(START_POSITION);
+ 
+ }
 }
